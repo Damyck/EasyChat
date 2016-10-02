2aa52a3e50b0cfe3dd088d21e5fa1773d7d9e279
Marc <marcjb2@gmail.com>
2016-10-02 13:12:03 +0200

    First Commit

---
+++ src/ClientMain.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by Marc on 2/10/2016.
+ */
+public class ClientMain {
+    public static void main(String[] args)throws IOException{
+        ChatClient cl = new ChatClient("localhost", 4444);
+        cl.start();
+    }
+}
